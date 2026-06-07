0c68faa038f951238269e0b4545836c6377d70e6
gabriel03013 <gm5770283@gmail.com>
2026-06-07 18:35:56 -0300

    feat/upload initial files to github

---
+++ src/test/java/com/frigus/bffweb/BffWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frigus.bffweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
